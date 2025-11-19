34f94561ac76b25e689c35c31cac6979a5d6d0ee
Luke <s267071@uos.ac.uk>
2025-11-19 09:31:53 +0000

    Very simple movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private InputAction moveAction;
+    private Vector2 movementInput;
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get move action vector
+        movementInput = moveAction.ReadValue<Vector2>();
+        rb.linearVelocity = new Vector2(movementInput.x * moveSpeed, rb.linearVelocity.y);
+    }
+}
