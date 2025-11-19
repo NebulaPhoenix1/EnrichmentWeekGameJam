57ac2bfbc5709c7a0a3d9cd32b9d031d6b9c7bc3
Luke <s267071@uos.ac.uk>
2025-11-19 10:07:30 +0000

    Jumping!

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour
     private Vector2 movementInput;
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float jumpSpeed = 5f;
     private bool grounded = false;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,7 +24,13 @@ public class Movement : MonoBehaviour
     {
         //Get move action vector
         movementInput = moveAction.ReadValue<Vector2>();
+        //Horizontal Movement
         rb.linearVelocity = new Vector2(movementInput.x * moveSpeed, rb.linearVelocity.y);
+        //Jumping Logic
+        if(grounded && movementInput.y > 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, movementInput.y * jumpSpeed);
+        }
     }
 
     public void SetGrounded(bool value)
