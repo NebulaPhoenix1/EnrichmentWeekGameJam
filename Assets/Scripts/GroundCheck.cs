5fc9f50b565af02c7803a9d1abe1d15fd2404820
Luke <s267071@uos.ac.uk>
2025-11-19 15:27:43 +0000

    Better ground check with BoxCast

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -1,28 +1,22 @@
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GroundCheck : MonoBehaviour
 {
-    private Collider2D groundCheckColldier;
-    public UnityEvent grounded;
-    public UnityEvent notGrounded;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Vector2 boxcastOffset;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool CheckIfGrounded()
     {
-        groundCheckColldier = GetComponent<BoxCollider2D>();   
+        //Do a boxcast below the player
+        //It will return all the colliders within it
+        //If any are tagged ground, return true else false
+        Vector2 currentPosition = transform.position;
+        bool grounded = Physics2D.BoxCast((currentPosition + boxcastOffset), Vector2.one, 0, Vector2.down, 1, groundLayer);
+        Debug.Log(grounded);
+        return grounded;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        //Debug.Log("On Ground! (probably)");
-        grounded.Invoke();
-    }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        //Debug.Log("Off Ground (probably)");
-        notGrounded.Invoke();
-    }
+
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,17 +12,16 @@ public class Movement : MonoBehaviour
     [SerializeField] private float maxMoveSpeed = 10f;
     private float increaseSpeedTimer = 0f;
     [SerializeField] private float jumpSpeed = 5f;
-    private bool grounded = false;
     private Animator playerAnim;
+    private GroundCheck groundCheck;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();
-        grounded = true;
         playerAnim = GetComponent<Animator>();
-        
+        groundCheck = GetComponent<GroundCheck>();
     }
 
     // Update is called once per frame
@@ -46,21 +45,9 @@ public class Movement : MonoBehaviour
         //Horizontal Movement
         rb.linearVelocity = new Vector2(movementInput.x * moveSpeed, rb.linearVelocity.y);
         //Jumping Logic
-        if(grounded && movementInput.y > 0)
+        if(groundCheck.CheckIfGrounded() && movementInput.y > 0)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, movementInput.y * jumpSpeed);
-        }
-
-
-        
-    }
-
-    public void SetGrounded(bool value)
-    {
-        grounded = value;
-    }
-    public bool GetGrounded()
-    {
-        return grounded;
+        }   
     }
 }
